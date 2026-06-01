1f410516e4c16cd50a22d0acc4f8eeb2b576cea8
kybrott-glitch <kybrott@gmail.com>
2026-06-01 22:35:31 +0530

    Add files via upload

---
+++ hhss.py
@@ -0,0 +1,36 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(F.text)
+async def convert_emoji(message: Message):
+    if not message.entities:
+        return
+
+    for entity in message.entities:
+        if entity.type == "custom_emoji":
+            custom_emoji_id = entity.custom_emoji_id
+
+            sticker = await bot.get_custom_emoji_stickers(
+                custom_emoji_ids=[custom_emoji_id]
+            )
+
+            if not sticker:
+                await message.reply("Failed to get emoji.")
+                return
+
+            await message.reply_sticker(sticker[0].file_id)
+            return
+
+    await message.reply("Send a Telegram custom emoji.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
